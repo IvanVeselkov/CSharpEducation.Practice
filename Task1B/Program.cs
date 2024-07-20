7ad5d9723d58388b4fff5f205b705ce2cfe7df09
IvanVeselkov <32011610+IvanVeselkov@users.noreply.github.com>
2024-07-20 11:21:57 +0400

    Initial commit

---
+++ Task1B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        #region 1
+        ///// <summary>
+        ///// 1
+        ///// </summary>
+        //private int ageUser;
+        //private string nameUser;
+        //private string nameCompany;
+        //private bool logicUser;
+        //private float weightUser;
+        ////
+        #endregion
+        static void Main(string[] args)
+        {
+            #region 2
+            /////2
+            //int myAge = 26;
+            //string myName = "Ivan Veselkov";
+            //int temperatureOutsideCelsius = 26;
+            //bool areWoman = false;
+            ////
+            #endregion
+
+            #region 3
+            /////3
+            //Console.Write("Enter your name: ");
+            //string consoleMSGName = Console.ReadLine();
+            //Console.WriteLine(string.Format("Hello {0}", consoleMSGName));
+            #endregion
+
+            #region 4
+            //Console.Write("Enter your first phrase: ");
+            //string consoleMSGFirstPhrase = Console.ReadLine();
+            //Console.Write("Enter your second phrase: ");
+            //string consoleMSGSeconsPhrase = Console.ReadLine();
+            //Console.WriteLine(string.Format("{0} {1}", consoleMSGFirstPhrase,consoleMSGSeconsPhrase));
+            #endregion
+
+            #region 5
+            //const float Pi = 3.1415f;
+            //float R = 5f;
+            //float S = Pi * R * R;
+            //Console.WriteLine(string.Format("Площадь круга радиуса R({0}) равна: {1}", R, S));
+            #endregion
+
+            #region 6
+            ////Условие первое: Известны два катета
+            //double _1CatetA = 3;
+            //double _1CatetB = 4;
+            //double _1Gipo = Math.Sqrt(Math.Pow(_1CatetA, 2) + Math.Pow(_1CatetB, 2));
+            //Console.WriteLine(string.Format("Условие первое: Известны два катета. Необходимо найти гипотенузу. \n Катет A:{0} \n Катет B:{1} \n Гипотенуза С = {2}", _1CatetA, _1CatetB, _1Gipo));
+
+            //Условие второе: Известен катет и гипотенуза
+            double _2CatetA = 3;
+            double _2Gipo = 5;
+            double _2CatetB = Math.Sqrt(Math.Pow(_2Gipo, 2) - Math.Pow(_2CatetA, 2)); ;
+            Console.WriteLine(string.Format("Условие второе: Известен катет и гипотенуза. Необходимо найти второй катет. \n Катет A:{0} \n Гипотенуза С:{1} \n Катет B = {2}", _2CatetA, _2Gipo, _2CatetB));
+            #endregion
+        }
+        //
+
+
+    }
+}
